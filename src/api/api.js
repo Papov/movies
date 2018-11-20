d202cbad971c5f785da844e35f3993ca65c45749
Vlad Popov <vlad.linkoln.94@gmail.com>
2018-11-20 17:18:02 +0200

    first commit

---
+++ src/api/api.js
@@ -0,0 +1,6 @@
+export const API_URL = "https://api.themoviedb.org/3";
+
+export const API_KEY_3 = "f3b6bf0826b03d53c7be6ac7c02b226c";
+
+export const API_KEY_4 =
+  "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmM2I2YmYwODI2YjAzZDUzYzdiZTZhYzdjMDJiMjI2YyIsInN1YiI6IjViM2IyYjkzMGUwYTI2NmUzMDAxODM4MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.aTU0qAPwjdc_1Pvq4s-zLtLQzLJZTj-QvkxnW60jDms";
